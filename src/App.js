a6143f6c9d1addb79645a74fbb39c8404c8db81d
Duraan Ali <duraan522@gmail.com>
2019-08-06 17:36:40 -0500

    final mvp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Card from "./Components/Card";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: {},
+      followers: [],
+      currentUsername: "",
+      updatedUsername: ""
+    };
+  }
+
+  fetchGithubData = () => {
+    axios
+      .get("https://api.github.com/users/nicholas-patterson")
+      .then(res => this.setState({ userData: res.data }))
+      .catch(err => console.log(err));
+  };
+
+  fetchFollowersData = () => {
+    axios
+      .get("https://api.github.com/users/nicholas-patterson/followers")
+      .then(res => this.setState({ followers: res.data }))
+      .catch(err => console.log(err));
+  };
+
+  componentDidMount = () => {
+    this.fetchGithubData();
+    this.fetchFollowersData();
+  };
+
+  render() {
+    return (
+      <div>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <Typography variant="h6" color="primary">
+              React Github User Card
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <div>
+          <Card
+            userData={this.state.userData}
+            followers={this.state.followers}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
